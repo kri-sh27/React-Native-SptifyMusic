db44fde6b606da896a8e523e0408327d6a523dc6
kri-sh27 <hogalekrishna2000@gmail.com>
2023-08-24 17:08:55 +0530

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-
-
-import React,{useState,useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   ActivityIndicator,
@@ -12,47 +10,46 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-import {setupPlayer,addTrack}from"../musicPlayerServices"
+import {setupPlayer, addTrack} from '../musicPlayerServices';
 import MusicPlayer from './screens/MusicPlayer';
 
-
 function App(): JSX.Element {
-  const [isPlayerReady, setIsPlayerReady] = useState(false)
+  const [isPlayerReady, setIsPlayerReady] = useState(false);
 
-  async function setup(){
-    let isSetup = await setupPlayer()
+  async function setup() {
+    let isSetup = await setupPlayer();
 
     if (isSetup) {
-      await addTrack()
+      await addTrack();
     }
 
-    setIsPlayerReady(isSetup)
+    setIsPlayerReady(isSetup);
   }
 
-  useEffect(()=>{
-    setup()
-  },[])
+  useEffect(() => {
+    setup();
+  }, []);
 
   if (!isPlayerReady) {
     return (
       <SafeAreaView>
         <ActivityIndicator />
       </SafeAreaView>
-    )
+    );
   }
 
   return (
     <View style={styles.container}>
-      <StatusBar barStyle={"light-content"} />
+      <StatusBar barStyle={'light-content'} />
       <MusicPlayer />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-  }
+  container: {
+    flex: 1,
+  },
 });
 
 export default App;
